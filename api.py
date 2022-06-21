165f7b3a54f485e1a6a6c31ad8409cdc347a0359
mfaiqrofifi <mfaiqrofifi@gmail.com>
2022-06-21 18:55:36 +0700

    final

--- api.py
+++ api.py
@@ -3,20 +3,26 @@ import pandas as pd
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS,cross_origin
 import pickle
+import numpy as np
 
 app=Flask(__name__)
 
-CORS(app)
+#CORS(app)
 
 model = pickle.load(open("training2.pkl","rb"))
-
+@app.route('/')
+def home():
+    return render_template('tampilan.html')
 
 @app.route("/predict",methods=["POST"])
 def predict():
-    json_=request.json
-    query_df=pd.DataFrame(json_)
-    prediction=model.predict(query_df)
-    return jsonify({"Prediction":list(prediction.astype("str"))})
+    #json_=request.json
+    #query_df=pd.DataFrame(json_)
+    int_features=[x for x in request.form.values()]
+    #prediction=model.predict(query_df)
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    return render_template('tampilan.html', prediction_text='Status {}'.format(prediction[0]))
 
 if __name__=="__main__":
     app.run(debug=True)
